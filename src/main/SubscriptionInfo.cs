4d846facd1fbbe13e319f24016d080096edd06c0
Junvic <contact@junvic.me>
2022-06-12 07:21:21 +0800

    add DTOs for browser subscription

---
+++ src/main/SubscriptionInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ei8.Cortex.Subscriptions.Common
+{
+    public abstract class SubscriptionInfo
+    {
+        public Guid UserId { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
