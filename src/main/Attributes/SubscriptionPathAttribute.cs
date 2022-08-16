1961cabf2959194820f5f2c5f85d07312609ab72
Junvic <contact@junvic.me>
2022-08-17 04:36:29 +0800

    refactor: separate subscription info from receiver info

---
+++ src/main/Attributes/SubscriptionPathAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ei8.Cortex.Subscriptions.Common.Attributes
+{
+    /// <summary>
+    /// Specifies the relative endpoint path this class can be used with
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class SubscriptionPathAttribute : Attribute
+    {
+        /// <summary>
+        /// Initialize this SubscriptionPathAttribute with the specified relative endpoint path
+        /// </summary>
+        /// <param name="endpointPath">The path relative to the subscription endpoint, e.g. "subscriptions/receivers/{endpointPath}"</param>
+        public SubscriptionPathAttribute(string endpointPath)
+        {
+            EndpointPath = endpointPath;
+        }
+
+        /// <summary>
+        /// The path relative to the subscription endpoint, e.g. "subscriptions/receivers/{EndpointPath}"
+        /// </summary>
+        public string EndpointPath { get; }
+    }
+}
--- src/main/SubscriptionInfo.cs
+++ src/main/SubscriptionInfo.cs
@@ -2,7 +2,7 @@
 
 namespace ei8.Cortex.Subscriptions.Common
 {
-    public abstract class SubscriptionInfo
+    public class SubscriptionInfo
     {
         public Guid UserId { get; set; }
         public string AvatarUrl { get; set; }
