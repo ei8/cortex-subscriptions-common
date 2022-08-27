97b526356653713b497fd33a37c28079f78f0f57
Junvic <contact@junvic.me>
2022-08-27 09:27:19 +0800

    refactor: add extension method for parsing receiver info attribute

---
+++ src/main/Extensions/IReceiverInfoExtensions.cs
@@ -0,0 +1,23 @@
+﻿using ei8.Cortex.Subscriptions.Common.Attributes;
+using ei8.Cortex.Subscriptions.Common.Receivers;
+using System;
+using System.Linq;
+
+namespace ei8.Cortex.Subscriptions.Common.Extensions
+{
+    public static class IReceiverInfoExtensions
+    {
+        public static string GetSubscriptionPath(this IReceiverInfo receiverInfo)
+        {
+            var type = receiverInfo.GetType();
+            var subscriptionPathAttribute = type.GetCustomAttributes(typeof(SubscriptionPathAttribute), false)
+                                                .FirstOrDefault() as SubscriptionPathAttribute;
+
+            if (subscriptionPathAttribute != null)
+                return subscriptionPathAttribute.EndpointPath;
+
+            else
+                throw new ArgumentException($"Unsupported receiver info type: {type.FullName}");
+        }
+    }
+}
