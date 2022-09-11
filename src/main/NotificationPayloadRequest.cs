34bdc26ac651e87003249824ae7c2bcea604cddc
Junvic <contact@junvic.me>
2022-09-12 04:07:21 +0800

    feature: add DTO for notification payload requests

---
+++ src/main/NotificationPayloadRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ei8.Cortex.Subscriptions.Common
+{
+    public class NotificationPayloadRequest
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
