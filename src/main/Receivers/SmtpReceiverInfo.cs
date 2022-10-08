58130c36d113412052eaf09564a76be635a2f322
elmerbool <35117496+elmerbool@users.noreply.github.com>
2022-10-08 22:21:10 +1100

    Added SMTP.

---
+++ src/main/Receivers/SmtpReceiverInfo.cs
@@ -0,0 +1,16 @@
+﻿using ei8.Cortex.Subscriptions.Common.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ei8.Cortex.Subscriptions.Common.Receivers
+{
+    [SubscriptionPath("smtp")]
+    public class SmtpReceiverInfo : IReceiverInfo
+    {
+        /// <summary>
+        /// Specifies the email address.
+        /// </summary>
+        public string EmailAddress { get; set; }
+    }
+}
